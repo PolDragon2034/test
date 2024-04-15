93925223ecffb89e4a1b92694e92c92ebd71d318
PolDragon2034 <135629139+PolDragon2034@users.noreply.github.com>
2024-04-15 18:58:41 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,1 +1,10 @@
-print("Tdfg")
+def dodaj(a, b):
+    return a + b
+  
+import pytest
+from main import dodaj
+
+def test_dodaj():
+    assert dodaj(2, 3) == 5
+    assert dodaj(-1, 1) == 0
+    assert dodaj(0, 0) == 0
